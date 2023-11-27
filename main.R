616e1d023b6dd861d8ee563f063613c79ff9eed9
Losenok05 <122996681+Losenok05@users.noreply.github.com>
2023-11-26 18:01:47 -0800

    All datasets, I found + data merging in main.R

---
+++ main.R
@@ -0,0 +1,30 @@
+library(dplyr)
+
+#US_youtube_trending_data <- read.csv("US_youtube_trending_data.csv")
+#Global_Youtube_stats <- read.csv("Global YouTube Statistics.csv")
+#Global_Youtube_stats <- na.omit(Global_Youtube_stats)
+
+USvideos <- read.csv("USvideos.csv")
+GBvideos <- read.csv("GBvideos.csv")
+
+english_youtube_1 <- rbind(USvideos, GBvideos)
+
+
+USvideos2 <- read.csv("USvideos0.csv")
+GBvideos2 <- read.csv("GBvideos0.csv")
+
+GBcomments <- read.csv("GBcomments.csv")
+UScomments <- read.csv("UScomments.csv")
+
+all_comments <- rbind(GBcomments, UScomments)
+
+
+english_youtube_2 <- rbind(USvideos2, GBvideos2)
+english_youtube_2 <- subset(english_youtube_2, select = -thumbnail_link)
+
+df <- merge(x = english_youtube_2, y = all_comments, by = "video_id", all.y = TRUE)
+#comments <- read.csv("comments.csv")
+#video_stats <- read.csv("videos-stats.csv")
+
+#stats_and_comments <- merge(x = video_stats, y = comments, by.x = "Video.ID", by.y = "Video.ID")
+#stats_and_comments <- na.omit(stats_and_comments)

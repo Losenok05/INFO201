2851324813d57485c8cca911fe5a204f70b8f0d7
Losenok05 <122996681+Losenok05@users.noreply.github.com>
2023-11-27 21:10:09 -0800

    Update main.R

--- main.R
+++ main.R
@@ -20,4 +20,13 @@ english_youtube_2 <- subset(english_youtube_2, select = -thumbnail_link)
 df <- merge(x = english_youtube_2, y = all_comments, by = "video_id")
 # The data set turned out to be clean, but just in case:
 df <- na.omit(df)
-df_unique <- df[!duplicated(df), ]
+df <- df[!duplicated(df), ] # takes about 5 minutes to complete
+# additional columns: engagement rate and high engagemnt
+df$engagement_rate <- df$likes.x / df$views
+likes_threshold <- quantile(df$likes.x, 0.75)
+df$high_engagement <- ifelse(df$likes.x > likes_threshold, "High", "Low") # runs for 1 minute
+
+#summarisation df
+tag_list <- unlist(strsplit(as.character(df$tags), "\\|"))#one minute
+tag_table <- table(tag_list)
+tag_summary <- data.frame(tag = names(tag_table), frequency = as.integer(tag_table))

4698902c889a79a906bc6cff80ebef075191718b
Losenok05 <122996681+Losenok05@users.noreply.github.com>
2023-11-27 20:18:20 -0800

    l

--- main.R
+++ main.R
@@ -1,33 +1,23 @@
 library(dplyr)
 
-#US_youtube_trending_data <- read.csv("US_youtube_trending_data.csv")
-#Global_Youtube_stats <- read.csv("Global YouTube Statistics.csv")
-#Global_Youtube_stats <- na.omit(Global_Youtube_stats)
-
-#USvideos <- read.csv("USvideos.csv")
-#GBvideos <- read.csv("GBvideos.csv")
-
-#english_youtube_1 <- rbind(USvideos, GBvideos)
-
-
+# Reading YouTube video and comment datasets for US and GB
 USvideos2 <- read.csv("USvideos0.csv")
 GBvideos2 <- read.csv("GBvideos0.csv")
 GBcomments <- read.csv("GBcomments.csv")
 UScomments <- read.csv("UScomments.csv")
 
+# Combining comments datasets from GB and US
 all_comments <- rbind(GBcomments, UScomments)
 
-
+# Combining video datasets from US and GB
 english_youtube_2 <- rbind(USvideos2, GBvideos2)
+
+# Removing 'thumbnail_link' column from the videos dataset
 english_youtube_2 <- subset(english_youtube_2, select = -thumbnail_link)
 
-df <- merge(x = english_youtube_1, y = all_comments, by = "video_id")
+# Merging videos and comments datasets using 'video_id' as the key
+# and removing NA values and duplicate rows
+df <- merge(x = english_youtube_2, y = all_comments, by = "video_id")
+# The data set turned out to be clean, but just in case:
 df <- na.omit(df)
 df_unique <- df[!duplicated(df), ]
-
-#comments <- read.csv("comments.csv")
-#video_stats <- read.csv("videos-stats.csv")
-
-
-#stats_and_comments <- merge(x = video_stats, y = comments, by.x = "Video.ID", by.y = "Video.ID")
-#stats_and_comments <- na.omit(stats_and_comments)
